39dedc5ccbcb3f4987e46f67b0aeb05dcfc4888c
Joe Hacker <166112520+joeruhacker@users.noreply.github.com>
2025-07-03 07:21:24 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # --- секреты ---
-TELEGRAM_TOKEN = '7991938170:AAHtDQiGq4jEn-lLmSnJ4OU0yszFD_n6dUs'
-FASHN_API_KEY  = 'fa-VdHFUoqU9ahr-ru741yTqKlb25rLdRWQ86OFk'
+TELEGRAM_TOKEN = '------------------------------------------'
+FASHN_API_KEY  = '-------------------------------------------'
 
 # --- сеть ---
 # На тесте (ngrok) подменяйте это на https://<твой-.ngrok.app>
-WEBHOOK_URL_BASE = 'https://59ea-85-16-128-158.ngrok-free.app'   # ← поменяйте в продакшне
+WEBHOOK_URL_BASE = '----------------------------------------p'   # ← поменяйте в продакшне
 WEBHOOK_URL_PATH = f"/bot/{TELEGRAM_TOKEN}"
 
 # Локальный порт, который будете пробрасывать через ngrok
